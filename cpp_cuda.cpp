df671eb74eb7558e938e85ce018187444deca111
anzisheng <anzisheng@163.com>
2024-08-30 15:29:38 +0800

    turorial-1

---
+++ cpp_cuda.cpp
@@ -0,0 +1,23 @@
+#include <torch/extension.h>
+
+#include <iostream>
+
+using namespace std;
+torch::Tensor trilinear_interpolation(
+    torch::Tensor  feats,
+    torch::Tensor point 
+)
+{
+    return feats;
+
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
+    m.def("trilinear_interpolation", &trilinear_interpolation);
+    //m.def("trilinear_interpolation_bw", &trilinear_interpolation_bw);
+}
+// int main()
+// {
+//     cout << "hello" <<endl;
+//     return 0;
+// }
